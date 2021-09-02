7f78907d1e281c8e310d517179e6f06c08a37e2c
MizarZh <2267243023@qq.com>
2021-09-02 21:26:54 +0800

    feat: add antialias, DoubleSide rendering, FPS adjustment, debug info

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import moonData from './js/data';
+import $ from 'jquery';
 // import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass.js';
 
 // !初始化
@@ -11,7 +12,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 // const composer = new EffectComposer(renderer);
@@ -37,7 +38,12 @@ directionalLight.lookAt(0, 0, 0);
 // 全局光
 const ambientLight = new THREE.AmbientLight(0x222222);
 
-// !辅助
+scene.add(directionalLight);
+scene.add(ambientLight);
+scene.add(moonMesh);
+
+// !辅助 debug用
+
 // 坐标轴
 const axesHelper = new THREE.AxesHelper(5);
 
@@ -48,25 +54,22 @@ const lightMesh = new THREE.Mesh(lightMeshGeometry, lightMeshMaterial);
 
 // 赤道
 const circleGeometry = new THREE.RingGeometry(3, 4, 32);
-const circleMaterial = new THREE.MeshBasicMaterial({ color: 0x0066c });
+const circleMaterial = new THREE.MeshBasicMaterial({
+  color: 0x0066c,
+  side: THREE.DoubleSide,
+});
 const circle = new THREE.Mesh(circleGeometry, circleMaterial);
 circle.rotateX(-Math.PI / 2);
-const circle2 = new THREE.Mesh(circleGeometry, circleMaterial);
-circle2.rotateX(Math.PI / 2);
-
-scene.add(directionalLight);
 scene.add(axesHelper);
-scene.add(ambientLight);
-scene.add(moonMesh);
 scene.add(lightMesh);
 scene.add(circle);
-scene.add(circle2);
 
+// 摄像机初始位置
 camera.position.x = 10;
 camera.lookAt(0, 0, 0);
 
 // !数据
-// 转换函数
+// 转换函数，将longitude,latitude,r转换成三维坐标系
 const convert = (lon, lat, r) => {
   // if (lon > 180) lon = 360 - lon;
   lon = (Math.PI / 180) * lon;
@@ -95,12 +98,22 @@ let map = {
 // !动画
 let r = 10,
   i = 1,
-  n = moonData.data.length;
+  n = moonData.data.length,
+  FPS = 50,
+  renderDT = 1 / FPS,
+  timeS = 0;
 // console.log(moonData);
+let clock = new THREE.Clock();
 const animate = function () {
   requestAnimationFrame(animate);
-  if (i < n) {
+  // 帧率设定
+  let T = clock.getDelta();
+  timeS = timeS + T;
+  if (i < n && timeS > renderDT) {
     let frame = moonData.data[i];
+    // centerLon, centerLat: 月球正对观测者的经纬度
+    // lightLon, lightLat: 光照经纬度
+    // rotateAng: 北极轴倾斜角
     let centerLon = parseFloat(frame[map.centerLon]),
       centerLat = parseFloat(frame[map.centerLat]),
       lightLon = parseFloat(frame[map.lightLon]),
@@ -113,18 +126,19 @@ const animate = function () {
 
     directionalLight.position.set(...convert(lightLon, lightLat, r));
     directionalLight.lookAt(0, 0, 0);
-
-    lightMesh.position.set(...convert(lightLon, lightLat, 3));
-    // moonMesh.rotation.x += 0.01;
-    // moonMesh.rotation.y += 0.01;
-
-    // camera.position.x = r * Math.cos(t);
-    // camera.position.z = r * Math.sin(t);
-    // directionalLight.position.x = r * Math.cos(t);
-    // directionalLight.position.z = r * Math.sin(t);
-    // camera.position.y = r * Math.sin(t);
-
+    timeS = 0;
     i += 1;
+    // debug
+    lightMesh.position.set(...convert(lightLon, lightLat, 3));
+    $('.data').text(`
+      date:${frame[0]}
+      phase:${frame[7]}%
+      centerLon:${centerLon}
+      centerLat:${centerLat}
+      lightLon:${lightLon}
+      lightLat:${lightLat}
+      rotateAng:${rotateAng}
+      `);
   }
   renderer.render(scene, camera);
 };
